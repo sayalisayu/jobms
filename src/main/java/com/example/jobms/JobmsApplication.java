fe9d5695ed60290d382e4e886ee1a697c6fa7f26
User <sayali.aai1234@gmail.com>
2025-07-14 23:29:11 +0530

    Microservice

--- src/main/java/com/example/jobms/JobmsApplication.java
+++ src/main/java/com/example/jobms/JobmsApplication.java
@@ -2,8 +2,10 @@ package com.example.jobms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class JobmsApplication {
 
     public static void main(String[] args) {
